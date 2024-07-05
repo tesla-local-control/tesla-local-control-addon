2757874e6c59c7f638eaa9a0514e93550ae4ea6e
Pierre Belanger <baylanger@users.noreply.github.com>
2024-07-05 12:42:06 -0400

    Add libproduct.sh

---
+++ tesla_ble_mqtt/libproduct.sh
@@ -0,0 +1,20 @@
+#
+# Home Assistant Add-On product's library
+#
+function productInit() {
+
+  if [ -n "${HASSIO_TOKEN:-}" ]; then
+    export BLE_CMD_RETRY_DELAY="$(bashio::config 'ble_cmd_retry_delay')"
+    export BLE_MAC_LIST="$(bashio::config 'ble_mac3')"
+    export DEBUG="$(bashio::config 'debug')"
+    export MQTT_SERVER="$(bashio::config 'mqtt_server')"
+    export MQTT_PORT="$(bashio::config 'mqtt_port')"
+    export MQTT_USERNAME="$(bashio::config 'mqtt_username')"
+    export MQTT_PASSWORD="$(bashio::config 'mqtt_password')"
+    export PRESENCE_DETECTION_TTL="$(bashio::config 'presence_detection_ttl')"
+    export VIN_LIST="$(bashio::config 'vin_list')"
+  fi
+
+  export COLOR=true
+
+}
