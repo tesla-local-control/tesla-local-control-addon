6860e90d75881dc3c5f2a34ea9675d4078aa4182
Pierre Belanger <baylanger@users.noreply.github.com>
2024-07-07 12:22:30 -0400

    libproduct use config:exists, add new settings

--- tesla_ble_mqtt/libproduct.sh
+++ tesla_ble_mqtt/libproduct.sh
@@ -1,25 +1,51 @@
 #
 # Home Assistant Add-On product's library
 #
-function productInit() {
+function initProduct() {
 
-  if [ -n "${HASSIO_TOKEN:-}" ]; then
+  ### Required Configuration Settings
+  export BLE_MAC_LIST="$(bashio::config 'ble_mac_list')"
+  export MQTT_SERVER="$(bashio::config 'mqtt_server')"
+  export MQTT_PORT="$(bashio::config 'mqtt_port')"
+  export MQTT_USERNAME="$(bashio::config 'mqtt_username')"
+  export MQTT_PASSWORD="$(bashio::config 'mqtt_password')"
+  export VIN_LIST="$(bashio::config 'vin_list')"
+
+  ### Optional Configuration Settings
+  if bashio::config.exists 'ble_cmd_retry_delay'; then
     export BLE_CMD_RETRY_DELAY="$(bashio::config 'ble_cmd_retry_delay')"
-    export BLE_MAC_LIST="$(bashio::config 'ble_mac_list')"
+    else
+    export BLE_CMD_RETRY_DELAY=""
+  fi
+
+  if bashio::config.exists 'debug'; then
     export DEBUG="$(bashio::config 'debug')"
-    export MQTT_SERVER="$(bashio::config 'mqtt_server')"
-    export MQTT_PORT="$(bashio::config 'mqtt_port')"
-    export MQTT_USERNAME="$(bashio::config 'mqtt_username')"
-    export MQTT_PASSWORD="$(bashio::config 'mqtt_password')"
+  else
+    export DEBUG=""
+  fi
+
+  if bashio::config.exists 'presence_detection_ttl'; then
     export PRESENCE_DETECTION_TTL="$(bashio::config 'presence_detection_ttl')"
-    export VIN_LIST="$(bashio::config 'vin_list')"
+  else
+    export PRESENCE_DETECTION_TTL=""
+  fi
+
+  if bashio::config.exists 'presence_detection_loop_delay'; then
+    export PRESENCE_DETECTION_LOOP_DELAY="$(bashio::config 'presence_detection_loop_delay')"
+  else
+    export PRESENCE_DETECTION_LOOP_DELAY=""
+  fi
+
+  if bashio::config.exists 'ha_backend_disable'; then
+    export HA_BACKEND_DISABLE="$(bashio::config 'ha_backend_disable')"
+  else
+    export HA_BACKEND_DISABLE=""
   fi
 
   # Prevent bashio to complain for "unbound variable"
   export BLE_LN_LIST=""
   export BLECTL_FILE_INPUT=""
   export COLOR=true
-  export HA_BACKEND_DISABLE=false
   export PRESENCE_EXPIRE_TIME_LIST=""
 
 }
